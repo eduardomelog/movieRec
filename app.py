9cbe6f29761a6821e6e697e1f50436fcf147007e
Eduardo Melo <168614510+eduardomelog@users.noreply.github.com>
2024-08-10 12:11:23 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,50 @@
 import streamlit as st
 import pandas as pd
-from surprise import Dataset, KNNBasic
+import numpy as np
 
-# Cargar datos y modelo
-data = Dataset.load_builtin('ml-100k')
-trainset = data.build_full_trainset()
+# Cargar datos
 movies_df = pd.read_csv('https://files.grouplens.org/datasets/movielens/ml-100k/u.item', sep='|', encoding='ISO-8859-1', 
                         header=None, usecols=[0, 1], names=['movie_id', 'title'])
+ratings_df = pd.read_csv('https://files.grouplens.org/datasets/movielens/ml-100k/u.data', sep='\t', encoding='ISO-8859-1', 
+                         header=None, usecols=[0, 1, 2], names=['user_id', 'movie_id', 'rating'])
 
-sim_options = {
-    'name': 'cosine',
-    'user_based': True  # User-based filtering
-}
+# Crear una tabla dinámica con usuarios y calificaciones
+user_movie_matrix = ratings_df.pivot_table(index='user_id', columns='movie_id', values='rating')
 
-model = KNNBasic(sim_options=sim_options)
-model.fit(trainset)
+# Función para calcular las recomendaciones
+def get_recommendations(user_id, num_recommendations=5):
+    if user_id in user_movie_matrix.index:
+        user_ratings = user_movie_matrix.loc[user_id].dropna()
+        similar_users = user_movie_matrix.corrwith(user_ratings)
+        similar_users = similar_users.dropna().sort_values(ascending=False).head(10)
 
+        recommended_movies = pd.Series(dtype='float64')
+
+        for similar_user in similar_users.index:
+            similar_user_ratings = user_movie_matrix.loc[similar_user].dropna()
+            recommended_movies = recommended_movies.append(similar_user_ratings)
+
+        recommended_movies = recommended_movies.groupby(recommended_movies.index).mean()
+        recommended_movies = recommended_movies.drop(user_ratings.index, errors='ignore')
+        recommended_movies = recommended_movies.sort_values(ascending=False).head(num_recommendations)
+
+        return movies_df[movies_df['movie_id'].isin(recommended_movies.index)]
+    else:
+        return pd.DataFrame(columns=['title'])
+
+# Streamlit App
 st.title("Sistema de Recomendación de Películas")
 
 user_id = st.number_input("Ingrese su ID de usuario", min_value=1, max_value=943, step=1)
 
 if st.button("Recomendar"):
-    inner_user_id = trainset.to_inner_uid(str(user_id))
-    movies_watched = trainset.ur[inner_user_id]
-    watched_titles = [movies_df[movies_df['movie_id'] == int(trainset.to_raw_iid(i[0]))]['title'].values[0] for i in movies_watched]
-    
-    movies_not_watched = [i for i in trainset.all_items() if i not in [j[0] for j in movies_watched]]
-    predictions = [model.predict(str(user_id), trainset.to_raw_iid(i)) for i in movies_not_watched]
-    recommendations = sorted(predictions, key=lambda x: x.est, reverse=True)[:10]
-    
-    recommended_titles = [movies_df[movies_df['movie_id'] == int(rec.iid)]['title'].values[0] for rec in recommendations]
-    
-    st.write("Películas que el usuario ha visto:")
-    st.write(watched_titles)
-    
-    st.write("Películas recomendadas:")
-    st.write(recommended_titles)
+    recommendations = get_recommendations(user_id)
+    if not recommendations.empty:
+        st.write("Películas que el usuario ha visto:")
+        watched_titles = movies_df[movies_df['movie_id'].isin(user_movie_matrix.loc[user_id].dropna().index)]['title']
+        st.write(watched_titles.tolist())
+        
+        st.write("Películas recomendadas:")
+        st.write(recommendations['title'].tolist())
+    else:
+        st.write("No se encontraron recomendaciones para este usuario.")
